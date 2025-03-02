5cd4d4a6292b91f24b9a6b03571aece29a0e9c65
Andrew Mularo <50927268+andrewmularo@users.noreply.github.com>
2025-03-02 15:06:35 -0500

    Update male_het_10.sh

--- male_het_10.sh
+++ male_het_10.sh
@@ -3,9 +3,9 @@
 #SBATCH -N 1
 #SBATCH -n 1
 #SBATCH -t 1-00:00:00
-#SBATCH --error=malehet.err
-#SBATCH --output=malehet.out
-#SBATCH --job-name=produce_het_SLURMM_jobs
+#SBATCH --error=maletraithet.err
+#SBATCH --output=maletraithet.out
+#SBATCH --job-name=produce_het_trait_SLURMM_jobs
 
 
 while read -a line
@@ -23,16 +23,16 @@ module load bioinfo
 module load samtools
 
 #Move to the angsd_output folder containing folder
-cd /scratch/bell/amularo/LEPC/align_lepc_doublecheck/final_bams
+cd /scratch/bell/amularo/LEPC_rerun_feb2025/aligned/final_bams
 
 ### PCA1
 
-#samtools view -b -L pca1m.bed ${line[0]} > pca1m_${line[0]}
+samtools view -b -L pca1m.bed ${line[0]} > pca1m_${line[0]}
 
 
-#/depot/fnrdewoody/apps/angsd/angsd -i pca1m_${line[0]} -ref /scratch/bell/amularo/LEPC/Tympanuchus_pallidicinctus_reference/ref.fa  -anc /scratch/bell/amularo/LEPC/Tympanuchus_pallidicinctus_reference/ref.fa  -dosaf 1 -minQ 30 -minMapQ 30 -GL 1 -P 26 -out het/pca1m_${line[0]} -doCounts 1 -setMinDepth 3 
+/depot/fnrdewoody/apps/angsd/angsd -i pca1m_${line[0]} -ref /scratch/bell/amularo/LEPC_rerun_feb2025/LEPC_genome/ref.fa  -anc /scratch/bell/amularo/LEPC_rerun_feb2025/LEPC_genome/ref.fa  -dosaf 1 -minQ 30 -minMapQ 30 -GL 1 -P 26 -out het/pca1m_${line[0]} -doCounts 1 -setMinDepth 3 
 
-#/depot/fnrdewoody/apps/angsd/misc/realSFS het/pca1m_${line[0]}.saf.idx > het/pca1m_${line[0]}_est.ml
+/depot/fnrdewoody/apps/angsd/misc/realSFS het/pca1m_${line[0]}.saf.idx > het/pca1m_${line[0]}_est.ml
 
 
 ## Comb
@@ -40,7 +40,7 @@ cd /scratch/bell/amularo/LEPC/align_lepc_doublecheck/final_bams
 samtools view -b -L combm_10.bed ${line[0]} > combm_10_${line[0]}
 
 
-/depot/fnrdewoody/apps/angsd/angsd -i combm_10_${line[0]} -ref /scratch/bell/amularo/LEPC/Tympanuchus_pallidicinctus_reference/ref.fa  -anc /scratch/bell/amularo/LEPC/Tympanuchus_pallidicinctus_reference/ref.fa  -dosaf 1 -minQ 30 -minMapQ 30 -GL 1 -P 26 -out het/combm_10_${line[0]} -doCounts 1 -setMinDepth 3 
+/depot/fnrdewoody/apps/angsd/angsd -i combm_10_${line[0]} -ref /scratch/bell/amularo/LEPC_rerun_feb2025/LEPC_genome/ref.fa  -anc /scratch/bell/amularo/LEPC_rerun_feb2025/LEPC_genome/ref.fa  -dosaf 1 -minQ 30 -minMapQ 30 -GL 1 -P 26 -out het/combm_10_${line[0]} -doCounts 1 -setMinDepth 3 
 
 /depot/fnrdewoody/apps/angsd/misc/realSFS het/combm_10_${line[0]}.saf.idx > het/combm_10_${line[0]}_est.ml
 
@@ -50,7 +50,7 @@ samtools view -b -L combm_10.bed ${line[0]} > combm_10_${line[0]}
 samtools view -b -L culmenm_10.bed ${line[0]} > culmenm_10_${line[0]}
 
 
-/depot/fnrdewoody/apps/angsd/angsd -i culmenm_10_${line[0]} -ref /scratch/bell/amularo/LEPC/Tympanuchus_pallidicinctus_reference/ref.fa  -anc /scratch/bell/amularo/LEPC/Tympanuchus_pallidicinctus_reference/ref.fa  -dosaf 1 -minQ 30 -minMapQ 30 -GL 1 -P 26 -out het/culmenm_10_${line[0]} -doCounts 1 -setMinDepth 3 
+/depot/fnrdewoody/apps/angsd/angsd -i culmenm_10_${line[0]} -ref /scratch/bell/amularo/LEPC_rerun_feb2025/LEPC_genome/ref.fa  -anc /scratch/bell/amularo/LEPC_rerun_feb2025/LEPC_genome/ref.fa  -dosaf 1 -minQ 30 -minMapQ 30 -GL 1 -P 26 -out het/culmenm_10_${line[0]} -doCounts 1 -setMinDepth 3 
 
 /depot/fnrdewoody/apps/angsd/misc/realSFS het/culmenm_10_${line[0]}.saf.idx > het/culmenm_10_${line[0]}_est.ml
 
@@ -61,7 +61,7 @@ samtools view -b -L culmenm_10.bed ${line[0]} > culmenm_10_${line[0]}
 samtools view -b -L headm_10.bed ${line[0]} > headm_10_${line[0]}
 
 
-/depot/fnrdewoody/apps/angsd/angsd -i headm_10_${line[0]} -ref /scratch/bell/amularo/LEPC/Tympanuchus_pallidicinctus_reference/ref.fa  -anc /scratch/bell/amularo/LEPC/Tympanuchus_pallidicinctus_reference/ref.fa  -dosaf 1 -minQ 30 -minMapQ 30 -GL 1 -P 26 -out het/headm_10_${line[0]} -doCounts 1 -setMinDepth 3 
+/depot/fnrdewoody/apps/angsd/angsd -i headm_10_${line[0]} -ref /scratch/bell/amularo/LEPC_rerun_feb2025/LEPC_genome/ref.fa  -anc /scratch/bell/amularo/LEPC_rerun_feb2025/LEPC_genome/ref.fa  -dosaf 1 -minQ 30 -minMapQ 30 -GL 1 -P 26 -out het/headm_10_${line[0]} -doCounts 1 -setMinDepth 3 
 
 /depot/fnrdewoody/apps/angsd/misc/realSFS het/headm_10_${line[0]}.saf.idx > het/headm_10_${line[0]}_est.ml
 
@@ -72,7 +72,7 @@ samtools view -b -L headm_10.bed ${line[0]} > headm_10_${line[0]}
 samtools view -b -L massm_10.bed ${line[0]} > massm_10_${line[0]}
 
 
-/depot/fnrdewoody/apps/angsd/angsd -i massm_10_${line[0]} -ref /scratch/bell/amularo/LEPC/Tympanuchus_pallidicinctus_reference/ref.fa  -anc /scratch/bell/amularo/LEPC/Tympanuchus_pallidicinctus_reference/ref.fa  -dosaf 1 -minQ 30 -minMapQ 30 -GL 1 -P 26 -out het/massm_10_${line[0]} -doCounts 1 -setMinDepth 3 
+/depot/fnrdewoody/apps/angsd/angsd -i massm_10_${line[0]} -ref /scratch/bell/amularo/LEPC_rerun_feb2025/LEPC_genome/ref.fa  -anc /scratch/bell/amularo/LEPC_rerun_feb2025/LEPC_genome/ref.fa  -dosaf 1 -minQ 30 -minMapQ 30 -GL 1 -P 26 -out het/massm_10_${line[0]} -doCounts 1 -setMinDepth 3 
 
 /depot/fnrdewoody/apps/angsd/misc/realSFS het/massm_10_${line[0]}.saf.idx > het/massm_10_${line[0]}_est.ml
 
@@ -82,7 +82,7 @@ samtools view -b -L massm_10.bed ${line[0]} > massm_10_${line[0]}
 samtools view -b -L pinnaem_10.bed ${line[0]} > pinnaem_10_${line[0]}
 
 
-/depot/fnrdewoody/apps/angsd/angsd -i pinnaem_10_${line[0]} -ref /scratch/bell/amularo/LEPC/Tympanuchus_pallidicinctus_reference/ref.fa  -anc /scratch/bell/amularo/LEPC/Tympanuchus_pallidicinctus_reference/ref.fa  -dosaf 1 -minQ 30 -minMapQ 30 -GL 1 -P 26 -out het/pinnaem_10_${line[0]} -doCounts 1 -setMinDepth 3 
+/depot/fnrdewoody/apps/angsd/angsd -i pinnaem_10_${line[0]} -ref /scratch/bell/amularo/LEPC_rerun_feb2025/LEPC_genome/ref.fa  -anc /scratch/bell/amularo/LEPC_rerun_feb2025/LEPC_genome/ref.fa  -dosaf 1 -minQ 30 -minMapQ 30 -GL 1 -P 26 -out het/pinnaem_10_${line[0]} -doCounts 1 -setMinDepth 3 
 
 /depot/fnrdewoody/apps/angsd/misc/realSFS het/pinnaem_10_${line[0]}.saf.idx > het/pinnaem_10_${line[0]}_est.ml
 
@@ -93,7 +93,7 @@ samtools view -b -L pinnaem_10.bed ${line[0]} > pinnaem_10_${line[0]}
 samtools view -b -L tailm_10.bed ${line[0]} > tailm_10_${line[0]}
 
 
-/depot/fnrdewoody/apps/angsd/angsd -i tailm_10_${line[0]} -ref /scratch/bell/amularo/LEPC/Tympanuchus_pallidicinctus_reference/ref.fa  -anc /scratch/bell/amularo/LEPC/Tympanuchus_pallidicinctus_reference/ref.fa  -dosaf 1 -minQ 30 -minMapQ 30 -GL 1 -P 26 -out het/tailm_10_${line[0]} -doCounts 1 -setMinDepth 3 
+/depot/fnrdewoody/apps/angsd/angsd -i tailm_10_${line[0]} -ref /scratch/bell/amularo/LEPC_rerun_feb2025/LEPC_genome/ref.fa  -anc /scratch/bell/amularo/LEPC_rerun_feb2025/LEPC_genome/ref.fa  -dosaf 1 -minQ 30 -minMapQ 30 -GL 1 -P 26 -out het/tailm_10_${line[0]} -doCounts 1 -setMinDepth 3 
 
 /depot/fnrdewoody/apps/angsd/misc/realSFS het/tailm_10_${line[0]}.saf.idx > het/tailm_10_${line[0]}_est.ml
 
@@ -103,7 +103,7 @@ samtools view -b -L tailm_10.bed ${line[0]} > tailm_10_${line[0]}
 samtools view -b -L tarsusm_10.bed ${line[0]} > tarsusm_10_${line[0]}
 
 
-/depot/fnrdewoody/apps/angsd/angsd -i tarsusm_10_${line[0]} -ref /scratch/bell/amularo/LEPC/Tympanuchus_pallidicinctus_reference/ref.fa  -anc /scratch/bell/amularo/LEPC/Tympanuchus_pallidicinctus_reference/ref.fa  -dosaf 1 -minQ 30 -minMapQ 30 -GL 1 -P 26 -out het/tarsusm_10_${line[0]} -doCounts 1 -setMinDepth 3 
+/depot/fnrdewoody/apps/angsd/angsd -i tarsusm_10_${line[0]} -ref /scratch/bell/amularo/LEPC_rerun_feb2025/LEPC_genome/ref.fa  -anc /scratch/bell/amularo/LEPC_rerun_feb2025/LEPC_genome/ref.fa  -dosaf 1 -minQ 30 -minMapQ 30 -GL 1 -P 26 -out het/tarsusm_10_${line[0]} -doCounts 1 -setMinDepth 3 
 
 /depot/fnrdewoody/apps/angsd/misc/realSFS het/tarsusm_10_${line[0]}.saf.idx > het/tarsusm_10_${line[0]}_est.ml
 
@@ -114,13 +114,13 @@ samtools view -b -L tarsusm_10.bed ${line[0]} > tarsusm_10_${line[0]}
 samtools view -b -L wingm_10.bed ${line[0]} > wingm_10_${line[0]}
 
 
-/depot/fnrdewoody/apps/angsd/angsd -i wingm_10_${line[0]} -ref /scratch/bell/amularo/LEPC/Tympanuchus_pallidicinctus_reference/ref.fa  -anc /scratch/bell/amularo/LEPC/Tympanuchus_pallidicinctus_reference/ref.fa  -dosaf 1 -minQ 30 -minMapQ 30 -GL 1 -P 26 -out het/wingm_10_${line[0]} -doCounts 1 -setMinDepth 3 
+/depot/fnrdewoody/apps/angsd/angsd -i wingm_10_${line[0]} -ref /scratch/bell/amularo/LEPC_rerun_feb2025/LEPC_genome/ref.fa  -anc /scratch/bell/amularo/LEPC_rerun_feb2025/LEPC_genome/ref.fa  -dosaf 1 -minQ 30 -minMapQ 30 -GL 1 -P 26 -out het/wingm_10_${line[0]} -doCounts 1 -setMinDepth 3 
 
-/depot/fnrdewoody/apps/angsd/misc/realSFS het/wingm_10_${line[0]}.saf.idx > het/wingm_10_${line[0]}_est.ml" > /scratch/bell/amularo/LEPC/align_lepc_doublecheck/final_bams/jobs/windowsm_${line[0]}_het.sh
+/depot/fnrdewoody/apps/angsd/misc/realSFS het/wingm_10_${line[0]}.saf.idx > het/wingm_10_${line[0]}_est.ml" > /scratch/bell/amularo/LEPC_rerun_feb2025/aligned/final_bams/jobs/windowsm_${line[0]}_het.sh
 
 
 
-done < /scratch/bell/amularo/LEPC/align_lepc_doublecheck/final_bams/male_filelist.txt
+done < /scratch/bell/amularo/LEPC_rerun_feb2025/aligned/final_bams/male_filelist.txt
 
 
 
@@ -128,6 +128,6 @@ done < /scratch/bell/amularo/LEPC/align_lepc_doublecheck/final_bams/male_filelis
 
 while read -a sample
 do
-        cd /scratch/bell/amularo/LEPC/align_lepc_doublecheck/final_bams/errors
-        sbatch /scratch/bell/amularo/LEPC/align_lepc_doublecheck/final_bams/jobs/windowsm_${sample}_het.sh
-done < /scratch/bell/amularo/LEPC/align_lepc_doublecheck/final_bams/male_filelist.txt
+        cd /scratch/bell/amularo/LEPC_rerun_feb2025/aligned/final_bams/errors
+        sbatch /scratch/bell/amularo/LEPC_rerun_feb2025/aligned/final_bams/jobs/windowsm_${sample}_het.sh
+done < /scratch/bell/amularo/LEPC_rerun_feb2025/aligned/final_bams/male_filelist.txt
